edaac761a1ababec41a914a6ab7995e8b4ae9caf
rschalo <reed0schalo@gmail.com>
2019-09-07 19:09:54 -0700

    Dropped columns of unwanted data

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -2,5 +2,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('pd_collisions_datasd_v1.csv')
-
-print(df.count())
+#Open dataframe with collision data
+print(df.columns)
+df = df.drop(['violation_section', 'violation_type', 'charge_desc'], axis=1)
+#dropping columns with unwanted data
+print(df.columns)
